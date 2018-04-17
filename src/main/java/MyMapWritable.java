aefac9095b5497c3391bfc05d32cb0f72c2f0017
backslash112 <yangcun@live.com>
2018-04-17 17:59:11 -0500

    implement MapWritable to MyMapWritable, to override the toString() method for it

---
+++ src/main/java/MyMapWritable.java
@@ -0,0 +1,28 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.MapWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+
+import java.util.Map;
+
+public class MyMapWritable extends MapWritable {
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<Writable, Writable> item: this.entrySet()) {
+            Text key = (Text)item.getKey();
+            IntWritable value = (IntWritable)item.getValue();
+            sb.append("(");
+            sb.append(key.toString());
+            sb.append(":");
+            sb.append(value.toString());
+            sb.append("), ");
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return super.containsKey(key);
+    }
+}
