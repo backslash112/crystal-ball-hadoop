6dc34ec31ed353872eb7f25637660bf416023998
backslash112 <yangcun@live.com>
2018-04-17 21:08:48 -0500

    finish the pair with inmapper version

--- src/main/java/CrystalBall.java
+++ src/main/java/CrystalBall.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.HashMap;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -17,7 +18,14 @@ public class CrystalBall {
 
     public static class Map extends Mapper<LongWritable, Text, StringPair, IntWritable> {
         public static final Log log = LogFactory.getLog(Map.class);
-        private final static IntWritable one = new IntWritable(1);
+//        private final static IntWritable one = new IntWritable(1);
+
+        private java.util.Map<StringPair, Integer> H = null;
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            super.setup(context);
+            this.H = new HashMap<StringPair, Integer>();
+        }
 
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String line = value.toString();
@@ -30,18 +38,33 @@ public class CrystalBall {
             log.info("result.length: " + result.length);
             for (int i = 0; i < result.length; i++) {
                 log.info("i = " + i);
+
                 for (int j = i+1; j < result.length; j++) {
                     log.info("j = " + j);
                     if (result[j].equals(result[i])) {
                         log.info("break");
                         break;
                     }
-                    log.info(new StringPair(result[i], result[j]) + ", 1");
-                    context.write(new StringPair(result[i], result[j]), one);
+
+                    StringPair pair = new StringPair(result[i], result[j]);
+//                    log.info(pair + ", 1");
+//                    context.write(new StringPair(result[i], result[j]), one);
+                    int count = 1;
+                    if (this.H.containsKey(pair)) {
+                        count += this.H.get(pair);
+                    }
+                    this.H.put(pair, count);
                 }
             }
         }
 
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+            super.cleanup(context);
+            for (java.util.Map.Entry<StringPair, Integer> item: this.H.entrySet()) {
+                context.write(item.getKey(), new IntWritable(item.getValue()));
+            }
+        }
     }
     public static class Reduce extends Reducer<StringPair, IntWritable, StringPair, IntWritable> {
 
